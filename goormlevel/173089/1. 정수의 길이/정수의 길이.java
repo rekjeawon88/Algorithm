e1cac8ea6b53ae7cbc50fc74b58f865233d365b8
Kim Jaewon <rekjeawon88@naver.com>
2025-07-25 19:03:15 +0900

    [난이도 1] Title: 정수의 길이, Time: 0.14 ms, Memory: 34.88 MB -BaekjoonHub

---
+++ goormlevel/173089/1. 정수의 길이/정수의 길이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+
+		String a = scan.nextLine();
+
+		int result = a.length();
+		
+		System.out.println(result);
+	}
+}
