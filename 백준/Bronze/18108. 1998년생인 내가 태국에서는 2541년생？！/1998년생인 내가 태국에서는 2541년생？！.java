6d37ddfa84f72c6e445c026d61feaa5a7d604f3c
Kim Jaewon <rekjeawon88@naver.com>
2025-07-29 18:39:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int thaiAge = scan.nextInt();
+        
+        int age = thaiAge - 543;
+        
+        System.out.print(age);
+    }
+}
