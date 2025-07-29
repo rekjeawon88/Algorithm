0f10c894383134dff8c508d2aebe83f8cb2121b2
Kim Jaewon <rekjeawon88@naver.com>
2025-07-29 18:33:02 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        String name = scan.nextLine();
+        
+        System.out.printf("%s" + "??!", name);
+    }
+}
