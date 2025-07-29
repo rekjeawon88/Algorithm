22dfb9b14d3962b9b5a1a1fe189ea84686b08fbb
Kim Jaewon <rekjeawon88@naver.com>
2025-07-29 19:06:19 +0900

    [Bronze III] Title: 곱셈, Time: 180 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        
+        System.out.println(A * (B % 10));
+        System.out.println(A * ((B / 10) % 10));
+        System.out.println(A * (B / 100));
+        System.out.println(A * B);
+    }
+}
