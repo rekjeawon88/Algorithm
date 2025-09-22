e96e37d88510ecafd79e5af484f35f55d0e5e0c2
Kim Jaewon <rekjeawon88@naver.com>
2025-09-22 11:54:13 +0900

    더 간단화.java

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -2,21 +2,16 @@ class Solution {
     public String solution(String rsp) {
         String answer = "";
         
-        String[] tmp = rsp.split("");
-        int[] arr = new int[tmp.length];
-        
-        for(int i = 0; i < tmp.length; i++) {
-            arr[i] = Integer.parseInt(tmp[i]);
+        String[] arr = rsp.split("");
             
+        for(int i = 0; i < arr.length; i++) {
             switch (arr[i]) {
-                case 2:
-                    answer += '0';
+                case "2": answer += '0';
                     break;
-                case 0:
-                    answer += '5';
+                case "0": answer += '5';
                     break;
-                case 5:
-                    answer += '2';
+                case "5": answer += '2';
+                    break;
             }
         }
         
